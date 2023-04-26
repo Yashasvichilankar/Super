10e53bcacc8a1f2cb5f8995075568a1d766ca2a9
Yashasvi <yashasvichilankar27@gmail.com>
2023-04-26 18:15:40 +0530

    adding file

---
+++ Student17.java
@@ -0,0 +1,15 @@
+class Person{  
+void message(){System.out.println("welcome");}  
+}  
+  
+class Student17 extends Person{  
+  
+void display(){  
+super.message();//will invoke parent class message() method  
+}  
+  
+public static void main(String args[]){  
+Student17 s=new Student17();  
+s.display();  
+}  
+}  
