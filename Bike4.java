7ab5c68a43c9220eef2547b1a41b7227ad0cdafb
Yashasvi Chilankar <125111823+Yashasvichilankar@users.noreply.github.com>
2023-04-29 21:48:18 +0530

    Update Bike4.java

--- Bike4.java
+++ Bike4.java
@@ -7,7 +7,8 @@ class Bike4 extends Vehicle{
   int speed=100;  
       
   void display(){  
-   System.out.println(super.speed);//will print speed of Vehicle now  
+   System.out.println(super.speed);
+    //will print speed of Vehicle now  
   }  
   public static void main(String args[]){  
    Bike4 b=new Bike4();  
